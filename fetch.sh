00b4c5fb58a309a0f8b87ffbf2772ff7aed63695
tailuge <tailuge@gmail.com>
2023-05-23 08:32:34 +0000

    shellcheck

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/bash
-curl -r 0-1000000 https://database.lichess.org/standard/lichess_db_standard_rated_2023-03.pgn.zst --output raw.pgn.zst
-unzstd raw.pgn.zst --stdout > all.txt
-cat all.txt | sed '$ d' | grep -E "^1. " | sed 's/{[^}]*}//g' | sed -r 's/[0-9]+[.]+ //g' | sed 's/  / /g' | sed 's/[!?]*//g' | sed 's/ [012/-]*$//' | grep -v '*' | cut -d' ' -f1-16 > input.txt
-wc -l input.txt
-cat input.txt | sort | uniq | wc -l
+
+echo "Fetch data from lichess"
+echo ""
+
+mkdir -p data
+curl -r 0-1000000 https://database.lichess.org/standard/lichess_db_standard_rated_2023-03.pgn.zst --output data/raw.pgn.zst
+unzstd data/raw.pgn.zst --stdout > data/all.txt
+sed '$ d' data/all.txt | grep -E "^1. " | sed 's/{[^}]*}//g' | sed -r 's/[0-9]+[.]+ //g' | sed 's/  / /g' | \
+    sed 's/[!?]*//g' | sed 's/ [012/-]*$//' | \
+    grep -v '\*' | cut -d' ' -f1-16 | \
+    sort | uniq > data/input.txt
+
+wc -l data/input.txt
--- train.py
+++ train.py
@@ -19,12 +19,16 @@ dropout = 0
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open('data/input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
+
+print(chars)
+print(vocab_size)
+
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
