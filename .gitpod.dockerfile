20093891f0615b5fe2b847d3833fc23c509ea6b1
tailuge <tailuge@gmail.com>
2023-05-29 06:48:19 +0000

    env

--- .gitpod.dockerfile
+++ .gitpod.dockerfile
@@ -6,6 +6,6 @@ RUN echo "Start version from full"
 ### Python ###
 USER gitpod
 RUN sudo install-packages zstd shellcheck
-RUN pip install --upgrade pip && pip install numpy && pip install torch && pip install onnx && sudo rm -rf /tmp/*USER gitpod
+RUN pip install --upgrade pip && pip install numpy && pip install torch && pip install onnx && pip install onnxruntime && sudo rm -rf /tmp/*USER gitpod
 
 
---
+++ load.py
@@ -0,0 +1,20 @@
+import onnx
+import onnxruntime as rt
+
+model = onnx.load("./model/chessgpt.onnx")
+sess = rt.InferenceSession(model.SerializeToString())
+
+input_name = sess.get_inputs()[0].name
+print("input name", input_name)
+input_shape = sess.get_inputs()[0].shape
+print("input shape", input_shape)
+input_type = sess.get_inputs()[0].type
+print("input type", input_type)
+
+
+output_name = sess.get_outputs()[0].name
+print("output name", output_name)
+output_shape = sess.get_outputs()[0].shape
+print("output shape", output_shape)
+output_type = sess.get_outputs()[0].type
+print("output type", output_type)
