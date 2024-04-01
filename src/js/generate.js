4e0536d968d2aa67e3911378e7fc12849b9b6cb4
tailuge <tailuge@gmail.com>
2024-04-01 10:18:09 +0100

    deps

---
+++ src/js/generate.js
@@ -0,0 +1,6 @@
+const ort = require("onnxruntime-web")
+const tf = require("@tensorflow/tfjs")
+
+export function generateNextFrom(str, session) {
+  return "x"
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 const ort = require("onnxruntime-web")
 const tf = require("@tensorflow/tfjs")
-//const encdec = require("./encode.js")
+
+import { generateNextFrom } from "./generate"
 import { encodeString, decodeArray } from "./encode"
+
 async function main() {
   try {
     // create a new session and load the specific model.
@@ -18,11 +20,13 @@ async function main() {
     console.log(`outputNames:${outputNames}`)
 
     const prefix = "e4 e5 "
-    console.log(encodeString(prefix))
-    console.log(decodeArray(encodeString(prefix)))
+    const encoded = encodeString(prefix)
+    console.log(encoded)
+    console.log(decodeArray(encoded))
 
     const blockSize = 32
     const dataA = new BigInt64Array(blockSize)
+    encoded.forEach((x,i)=>dataA[i]=BigInt(x))
     console.log(dataA)
     const tensorA = new ort.Tensor("int64", dataA, [1, 32])
 
@@ -48,7 +52,6 @@ async function main() {
     console.log("lastRow.shape:", lastRow.shape)
     const shapedLastRow = lastRow.reshape([29])
     shapedLastRow.print()
-    shapedLastRow.print()
 
     // TensorflowJS to sample from the last row of the logits
 
@@ -60,9 +63,16 @@ async function main() {
     const idx_next = tf.multinomial(softmax, 1) // Shape: [B, 1]
 
     idx_next.print()
+
+    const tensorData = idx_next.dataSync();
+    console.log(tensorData)
+    var a = [tensorData[0]]
+    console.log(a)
+    console.log(`Decoded as ${decodeArray(a)}`)
     // read from results
     //        const dataC = results.c.data;
     //        document.write(`data of result tensor 'c': ${dataC}`);
+    console.log(generateNextFrom("e4 e5 ",session))
   } catch (e) {
     console.log(`failed inference ONNX model: ${e}.`)
     document.write(`failed inference ONNX model: ${e}.`)
